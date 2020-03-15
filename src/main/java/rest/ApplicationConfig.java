93592ca75e36ec136247290179a6114ff75d0694
Alexander Sarson <Alexander_Sarson@Outlook.com>
2020-03-15 14:47:17 +0100

    first commit

---
+++ src/main/java/rest/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package rest;
+
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+@javax.ws.rs.ApplicationPath("api")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        
+        //These two Resource Classes are not auto discovered so we add them manually
+        resources.add(OpenApiResource.class);
+        resources.add(AcceptHeaderOpenApiResource.class);
+
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(errorhandling.GenericExceptionMapper.class);
+        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+        resources.add(rest.RenameMeResource.class);
+    }
+
+}
