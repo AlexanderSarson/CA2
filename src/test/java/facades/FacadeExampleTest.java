415b0689a6d428d5b38f5a20b1c6001f90f9c43b
Alexander Sarson <Alexander_Sarson@Outlook.com>
2020-03-15 15:57:07 +0100

    Fixed deployment server for travis

--- src/test/java/facades/FacadeExampleTest.java
+++ src/test/java/facades/FacadeExampleTest.java
@@ -9,13 +9,14 @@ import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import utils.Settings;
 import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
+@Disabled
 public class FacadeExampleTest {
 
     private static EntityManagerFactory emf;
